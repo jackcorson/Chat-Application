3ba001cb48439e15aa717fbd7297d847ceaeb659
Jack Corson <jackcorson@Jacks-MacBook-Air-6.local>
2024-07-28 21:12:05 -0400

    Inital Server and Client code

---
+++ Client.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.net.*;
+ 
+public class Client extends Thread {
+    
+    private Socket socket;
+    private BufferedReader input;
+    private DataInputStream inputFromServer;
+    private DataOutputStream output;
+    
+    public Client(String address, int portNum) {
+        try {
+            socket = new Socket(address, portNum);
+            System.out.println("Connected to Server");
+
+            input = new BufferedReader(new InputStreamReader(System.in));
+            inputFromServer = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            output = new DataOutputStream(socket.getOutputStream());
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+
+        String msg = "";
+        new Thread(new IncomingMessages()).start();
+
+        while (!msg.equals("Bye")) {
+            try {
+                msg = input.readLine();
+                output.writeUTF(msg);
+            }
+            catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+
+        try {
+            socket.close();
+            input.close();
+            output.close();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+ 
+    public static void main(String args[])
+    {
+        Client client = new Client("127.0.0.1", 5060);
+    }
+
+    private class IncomingMessages implements Runnable {
+        @Override
+        public void run() {
+            String msgFromServer;
+            try {
+                while ((msgFromServer = inputFromServer.readUTF()) != null) {
+                    System.out.println(msgFromServer);
+                }
+            } catch (IOException e) {
+                System.out.println("Error reading from server: " + e.getMessage());
+            }
+        }
+    }
+}
---
+++ Server.java
@@ -0,0 +1,72 @@
+import java.net.*;
+import java.util.*;
+import java.io.*;
+
+public class Server {
+    private ServerSocket            server;
+    private Socket                  socket;
+    private ArrayList<HandleClient> clients = new ArrayList<HandleClient>();
+    
+    public Server(int portNum) {
+        try {
+            server = new ServerSocket(portNum);
+            System.out.println("Server created, waiting for a connection...");
+
+            while (true) {
+                socket = server.accept();
+                System.err.println("Connected!");
+                HandleClient client = new HandleClient(socket);
+                clients.add(client);
+                client.start();
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+ 
+    public static void main(String args[])
+    {
+        Server server = new Server(5060);
+    }
+
+    class HandleClient extends Thread {
+        private Socket           socket;
+        private DataInputStream  input;
+        private DataOutputStream output;
+    
+        public HandleClient(Socket socket) {
+            try {
+                this.socket = socket;
+                input = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+                output = new DataOutputStream(socket.getOutputStream());
+            }
+            catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    
+        public void run() {
+            String msgReceived = "";
+            try {
+                while (!msgReceived.equals("Bye")) {
+                    msgReceived = input.readUTF();
+                    System.out.println("Client " + socket.getRemoteSocketAddress() + ": " + msgReceived);
+                    for (HandleClient client : clients) {
+                        if (client != this) {
+                            client.output.writeUTF("From " + socket.getRemoteSocketAddress().toString() + ": " + msgReceived);
+                            client.output.flush();
+                        }
+                    }
+                }
+                input.close();
+                socket.close();
+                output.close();
+            } 
+            catch (IOException e) {
+                System.out.println(e);
+            }
+            clients.remove(this);
+        }
+    }
+}
